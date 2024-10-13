6cd3857de8f57f788cdb595ca7c58966d3bce1df
RenYan Wei <weirenyan@hotmail.com>
2024-10-13 18:54:16 +0800

    feat: add .gitkeep file

--- crates/leetcode/src/solution_0002.rs
+++ crates/leetcode/src/solution_0002.rs
@@ -25,3 +25,23 @@ impl Solution {
         result
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    // 注意这个惯用法：在 tests 模块中，从外部作用域导入所有名字。
+    use super::*;
+
+    #[test]
+    fn solution_test() {
+        let nums = vec![3, 5, 6];
+        let target = 9;
+
+        println!("输入参数 nums：{:?}", nums);
+        println!("输入参数target：{}", target);
+
+        let r = Solution::two_sum(nums, target);
+
+        println!("result:{:?}", r)
+    }
+}

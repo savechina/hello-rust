4c6c3c7ab0c520273a2e89e94f19b76a7451a578
RenYan Wei <weirenyan@hotmail.com>
2025-04-23 08:55:08 +0800

    feat: add sysinfo sample

---
+++ src/advance/sysinfo_sample.rs
@@ -0,0 +1,24 @@
+use sysinfo::System;
+
+/// .sysinfo example
+fn sysinfo_sample() {
+    let mut system = System::new_all();
+    system.refresh_all();
+
+    println!("System:{}", system.available_memory());
+
+    println!("Operating System: {:?}", System::name());
+    println!("OS Version: {:?}", System::os_version());
+    println!("Kernel Version: {:?}", System::kernel_version());
+    println!("Long OS Version: {:?}", System::long_os_version());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sysinfo_sample() {
+        sysinfo_sample();
+    }
+}

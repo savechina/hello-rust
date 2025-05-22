fde3e61e42b54c9aca2bf4e9dce7c880c87b0aad
RenYan Wei <weirenyan@hotmail.com>
2025-05-22 08:44:46 +0800

    feat: Update gRPC server to accept dynamic host and port arguments

--- src/bin/grpc_store_server.rs
+++ src/bin/grpc_store_server.rs
@@ -3,11 +3,17 @@ use clap::{Parser, Subcommand};
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+    #[arg(default_value = "127.0.0.1", long)]
+    host: String,
+    #[arg(default_value = "9001", short, long)]
+    port: u32,
 }
 fn main() {
     println!("Hello,Tonic Store server!");
-    tonic_store_server::store_server("127.0.0.1", 9001);
+    let opts = Args::parse();
+    println!("Host: {}", opts.host);
+    println!("Port: {}", opts.port);
+    println!("This is a gRPC server for the Tonic Store client.");
+
+    tonic_store_server::store_server(&opts.host, opts.port);
 }

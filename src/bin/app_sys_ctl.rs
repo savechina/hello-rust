b62b4c8fbc612bea3a8a90f47e52985b04196b3f
RenYan Wei <weirenyan@hotmail.com>
2025-08-05 23:34:15 +0800

    feat: add app ctl grace shutdown sample

--- src/bin/app_sys_ctl.rs
+++ src/bin/app_sys_ctl.rs
@@ -1,17 +1,29 @@
 use clap::{Parser, Subcommand};
-
+use http_body_util::combinators::BoxBody;
 use http_body_util::BodyExt;
+use http_body_util::Full;
+use hyper::body::Body;
+use hyper::body::Bytes;
+use hyper::body::Incoming;
+use hyper::server::conn::http1;
 use hyper::server::conn::http1::Builder;
 use hyper::service::service_fn;
+use hyper::service::HttpService;
 use hyper::service::Service;
-
+use hyper::Request;
+use hyper::Response;
+use hyper_util::rt::TokioIo;
 use hyper_util::server::graceful::GracefulShutdown;
-
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::time::Duration;
+use tokio::net::TcpListener;
+use tokio::pin;
 // use hyper::{Body, Request, Response};
 use log::{error, info};
+use rkyv::ser;
 use std::fs::{self, File};
 use std::io::Write;
-use std::net::SocketAddr;
 use std::path::Path;
 use sysinfo::{Pid, System};
 use tokio::signal;
@@ -71,21 +83,66 @@ async fn start_app() -> Result<(), Box<dyn std::error::Error>> {
     let (tx, rx) = oneshot::channel::<()>();
 
     // 定义简单的 HTTP 服务
-    // let make_svc = make_service_fn(|_conn| async {
-    //     Ok::<_, hyper::Error>(service_fn(|_req: Request<Body>| async {
-    //         Ok::<_, hyper::Error>(Response::new(Body::from("Hello, World!")))
-    //     }))
-    // });
+
+    // async fn hello(_: Request<Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    //     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+    // }
+
+    let hello = |_: Request<Incoming>| async {
+        Ok::<_, Infallible>(Response::new(Full::new(Bytes::from("Hello, World!"))))
+    };
 
     // 启动 HTTP 服务器
-    // let server = Server::bind(&addr)
-    //     .serve(make_svc)
-    //     .with_graceful_shutdown(async {
-    //         rx.await.ok();
-    //         info!("Received shutdown signal, stopping server...");
-    //     });
 
-    // info!("Server running on http://{}", addr);
+    let listener = TcpListener::bind(addr).await?;
+
+    info!("Server running on http://{}", addr);
+
+    let connection_timeouts = vec![Duration::from_secs(5), Duration::from_secs(2)];
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+
+        // Clone the connection_timeouts so they can be passed to the new task.
+        let connection_timeouts_clone = connection_timeouts.clone();
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Pin the connection object so we can use tokio::select! below.
+            let conn = http1::Builder::new().serve_connection(io, service_fn(hello));
+            pin!(conn);
+
+            // Iterate the timeouts.  Use tokio::select! to wait on the
+            // result of polling the connection itself,
+            // and also on tokio::time::sleep for the current timeout duration.
+            for (iter, sleep_duration) in connection_timeouts_clone.iter().enumerate() {
+                println!("iter = {} sleep_duration = {:?}", iter, sleep_duration);
+
+                tokio::select! {
+                    res = conn.as_mut() => {
+                        // Polling the connection returned a result.
+                        // In this case print either the successful or error result for the connection
+                        // and break out of the loop.
+                        match res {
+                            Ok(()) => println!("after polling conn, no error"),
+                            Err(e) =>  println!("error serving connection: {:?}", e),
+                        };
+                        break;
+                    }
+                    _ = tokio::time::sleep(*sleep_duration) => {
+                        // tokio::time::sleep returned a result.
+                        // Call graceful_shutdown on the connection and continue the loop.
+                        println!("iter = {} got timeout_interval, calling conn.graceful_shutdown", iter);
+                        conn.as_mut().graceful_shutdown();
+                    }
+                }
+            }
+        });
+    }
 
     // // 保存服务器任务
     // let server_task = tokio::spawn(async move {

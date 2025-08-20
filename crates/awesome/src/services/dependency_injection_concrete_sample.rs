c4d2e39c07e76b0fbbc038e11bb417ac13eb517d
RenYan Wei <weirenyan@hotmail.com>
2025-08-20 23:09:00 +0800

    feat: add dependency injection sample

--- crates/awesome/src/services/dependency_injection_concrete_sample.rs
+++ crates/awesome/src/services/dependency_injection_concrete_sample.rs
@@ -49,8 +49,8 @@ impl<R: UserRepository> UserService<R> {
     }
 }
 
-#[tokio::main] // di 通常与异步运行时配合，但这里可以不用 async
-async fn dependency_injection_manul_sample() -> anyhow::Result<()> {
+// di 通常与异步运行时配合，但这里可以不用 async
+fn dependency_injection_manul_sample() -> anyhow::Result<()> {
     // --- 创建 DI 容器 ---
     let mut services = HashMap::<TypeId, Arc<dyn Any + Send + Sync>>::new();
 

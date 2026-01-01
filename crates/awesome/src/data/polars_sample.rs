1f45156f3d3b198d4590fbdca3a9d15156450a83
RenYan Wei <weirenyan@hotmail.com>
2026-01-01 22:04:47 +0800

    feat: add polars sql sample

--- crates/awesome/src/data/polars_sample.rs
+++ crates/awesome/src/data/polars_sample.rs
@@ -1,4 +1,5 @@
 use polars::prelude::*;
+use polars::sql::SQLContext;
 
 fn polars_frame_sample() -> PolarsResult<()> {
     // 1. 创建 DataFrame
@@ -30,6 +31,49 @@ fn polars_frame_sample() -> PolarsResult<()> {
     Ok(())
 }
 
+/// polars dataframe handle by sql
+fn polars_sql_sample() -> PolarsResult<()> {
+    // 1. Prepare your data
+    let df = df!(
+        "name" => &["Alice", "Bob", "Charlie", "David"],
+        "age" => &[25, 30, 35, 28],
+        "salary" => &[5000, 8000, 12000, 7000]
+    )?;
+
+    // 2. Create a SQL Context and register the DataFrame as a table
+    let mut ctx = SQLContext::new();
+    ctx.register("users", df.lazy());
+
+    // 3. Execute a SQL query
+    let query = "
+        SELECT name, age, salary * 1.1 AS increased_salary
+        FROM users
+        WHERE age > 25
+        ORDER BY salary DESC
+    ";
+
+    let result = ctx.execute(query)?.collect()?;
+
+    println!("{:?}", result);
+
+    let lf = ctx
+        .execute(
+            "WITH high_earners AS (
+            SELECT * FROM users WHERE salary > 10000
+            )
+        SELECT name FROM high_earners",
+        )?
+        .collect()?;
+
+    println!("{:?}", lf);
+
+    let lf = ctx.execute("SELECT * FROM users")?;
+    let final_df = lf.with_column(col("age")).collect()?;
+    println!("{:?}", final_df);
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -37,5 +81,10 @@ mod tests {
     #[test]
     fn test_polars_frame_sample() {
         polars_frame_sample().unwrap();
+    }
+
+    #[test]
+    fn test_polars_sql_sample() {
+        polars_sql_sample().unwrap();
     }
 }
